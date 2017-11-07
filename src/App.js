b09754d9231130388ba5a2f69a35f48a8dca1626
pranavj1001 <pranavj1001@gmail.com>
2017-11-07 14:26:32 +0530

    wire NewsList to App.js

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
 import { Header } from './components/common';
+import NewsList from './components/NewsList';
 
 const App = () => {
   return (
     <Provider store={createStore(reducers)}>
       <View>
-      <Header headerText="Tech News" />
+        <Header headerText="Tech News" />
+        <NewsList />
       </View>
     </Provider>
   );
