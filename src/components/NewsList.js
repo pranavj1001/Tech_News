399bbaa8447b0274c965625e52cc5d774edf108e
pranavj1001 <pranavj1001@gmail.com>
2017-11-07 14:26:15 +0530

    add NewsList component

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { ListView } from 'react-native';
+import { NewsItem } from './NewsItem';
 
 class NewsList extends Component {
-  render() {
-    return;
+
+  componentWillMount() {
+    const ds = new ListView.DataSource({
+      rowsHasChanged: (r1, r2) => r1 !== r2
+    });
+    this.dataSource = ds.cloneWithRows(this.props.news);
   }
+
+  renderRow(news) {
+    return <NewsItem news={news} />;
+  }
+
+  render() {
+    console.log(this.props);
+    return (
+      <ListView
+        dataSource={this.dataSource}
+        renderRow={this.renderRow}
+      />
+    );
+  }
+
 }
 
-export default connect()(NewsList);
+const mapStateToProps = state => {
+  return { news: state.news };
+};
+
+export default connect(mapStateToProps)(NewsList);
