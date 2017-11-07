382fce8dd54bf97ec647baf5346702b61d7e0607
pranavj1001 <pranavj1001@gmail.com>
2017-11-07 12:34:06 +0530

    add NewsList component and get data from news api

---
+++ src/components/NewsList.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class NewsList extends Component {
+  render() {
+    return;
+  }
+}
+
+export default connect()(NewsList);
--- src/reducers/NewsReducer.js
+++ src/reducers/NewsReducer.js
@@ -1,1 +1,14 @@
-export default () => [];
+import axios from 'axios';
+
+class getData {
+
+  returnData() {
+    axios({
+        method: 'get',
+        url: 'https://newsapi.org/v1/articles?source=hacker-news&sortBy=top&apiKey=bdbcae478d174e04ba1504b2914cc143'
+    }).then(response => response.articles);
+  }
+
+}
+
+export default getData.returnData;
