7f108dd56c492854792fa7663cea08359e2b7f2e
pranavj1001 <pranavj1001@gmail.com>
2017-11-07 14:27:41 +0530

    add some under developement reducer code

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,12 @@
 import { combineReducers } from 'redux';
-import NewsReducer from './NewsReducer';
+import axios from 'axios';
+
+const NewsReducer = () => {
+  axios({
+      method: 'get',
+      url: 'https://newsapi.org/v1/articles?source=hacker-news&sortBy=top&apiKey=bdbcae478d174e04ba1504b2914cc143'
+  }).then(response => response.articles);
+};
 
 export default combineReducers({
   news: NewsReducer
