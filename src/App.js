f44dc3a8bf225abb1e15ffb963049f45c876d9c5
pranavj1001 <pranavj1001@gmail.com>
2017-11-09 00:19:09 +0530

    fix this bug (check description)
    <Provider> does not support changing `store` on the fly

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ import reducers from './reducers';
 import { Header } from './components/common';
 import NewsList from './components/NewsList';
 
+const store = createStore(reducers);
+
 const App = () => {
   return (
-    <Provider store={createStore(reducers)}>
+    <Provider store={store}>
       <View>
         <Header headerText="Tech News" />
         <NewsList />
