b8c395b519e3899b94ffe61b79857a1115b0b232
pranavj1001 <pranavj1001@gmail.com>
2017-11-10 23:26:55 +0530

    add SelectionReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import NewsReducer from './NewsReducer';
+import SelectionReducer from './SelectionReducer';
 
 export default combineReducers({
-  news: NewsReducer
+  news: NewsReducer,
+  selectedNewsId: SelectionReducer
 });
